6f902d33965cd01caefdc331bd272a2ecd9355f3
geraldkachi <fitzgeraldkachi@gmail.com>
2023-05-02 05:01:13 +0100

    latest

--- src/components/placeorder/PlaceOrder.tsx
+++ src/components/placeorder/PlaceOrder.tsx
@@ -40,7 +40,7 @@ const PlaceOrder = ({ setStateNew }: Props) => {
               <div className="text-[#031744] text-4xl font-semibold">Starlink</div>
 
               <p className="mt-5 text-[#031744]">
-                High-speed, low-latency broadband internet in remote and rural locations across the globe. NGN19,260/mo with a one-time hardware cost of NGN268,584.
+                High-speed, low-latency broadband internet in remote and rural locations across the globe. NGN19,260/mo with a one-time hardware cost of NGN{count * costFee}.
               </p>
 
               <div className="my-5">
