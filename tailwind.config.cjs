65a19b2a34e65a2a1111e42f0abeef54a560dc41
geraldkachi <fitzgeraldkachi@gmail.com>
2023-05-02 17:17:12 +0100

    latest fixes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,9 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      display: ["Plus Jakarta Sans", "Neue Haas Grotesk Display Pro", "Manrope"],
+    },
     extend: {},
   },
   plugins: [],
